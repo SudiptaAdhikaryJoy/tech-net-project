ee1608d390b450a663f35bc9f27e20bcc0caab9d
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-12-10 23:42:05 +0600

    fix: fix some sections

--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -17,7 +17,7 @@ const counterSlice = createSlice({
 			state.count += 2;
 		},
 		decrement: (state) => {
-			state.count -= 1;
+			state.count -= 2;
 		},
 		incrementByAmount: (state, action: PayloadAction<number>) => {
 			state.count += action.payload;
